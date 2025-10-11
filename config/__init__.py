b02e96614cadfddd4685abd7b32cb2819fce90c1
snakex21 <64422655+snakex21@users.noreply.github.com>
2025-10-12 01:20:00 +0200

    Delete config directory

--- config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Moduł konfiguracji Game Launcher
-Zawiera ustawienia, stałe i zarządzanie konfiguracją aplikacji.
-"""
-
-from .constants import *
-from .settings import Settings
-
-__all__ = ['Settings']
