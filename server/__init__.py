41c12af744399082f81a3a2be2991140cd9ef99d
snakex21 <64422655+snakex21@users.noreply.github.com>
2025-10-12 01:20:32 +0200

    Delete server directory

--- server/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Moduł serwera czatu
-Zawiera Flask server z Socket.IO dla real-time czatu.
-"""
-
-from .chat_server import ChatServer
-from .api import create_api_routes
-
-__all__ = ['ChatServer', 'create_api_routes']
